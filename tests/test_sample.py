0e0df28dec7c695147bc7d4864242c7f86197284
naresh-2026 <mannamnaresh9@gmail.com>
2025-03-17 01:03:05 +0530

    Update test_sample.py

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,5 +1,11 @@
 import pytest
-import app
+import sys
+import os
+
+# Ensure the app module is in the system path
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + "/.."))
+
+from app import app
 
 @pytest.fixture
 def client():
